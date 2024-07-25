6e679b688e332b320a3ffc313dd122d590792669
AsHuToShSiNgH02 <ashutoshin2002@gmail.com>
2024-07-25 21:15:52 +0530

    Added maincontent and updated UI for main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import './App.css';
 import SocialApp from './components/SocialApp';
-import PostCard from './components/PostCard'
 function App() {
 
   return (
     <>
         <SocialApp/>
-        <PostCard/>
     </>
   )
 }
